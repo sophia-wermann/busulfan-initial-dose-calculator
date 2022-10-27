77d5c25feef02d262ffe03bce866ae2b8b4c240d
Sophia Wermann <81588219+sophia-wermann@users.noreply.github.com>
2022-10-27 09:47:24 -0300

    Update var "vd" and var "cl" equations

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function calcularDose() {
     var aucNum = Number(auc.value);
     var aucTransformada = ((aucNum * 246.304) / 1000) / 60;
     
-    var vd = (8.17 * (1 + 0.0988 * (pesoNum - 13.65))) * Math.log(16);
-    var cl = (3.11 * (1 + 0.0198 * (idadeNum - 43))) * Math.log(39);
+    var vd = (8.61 * (1 + 0.0914 * (pesoNum - 13.65))) * Math.log(0.0195);
+    var cl = (3.17 * (1 + 0.0202 * (idadeNum - 43))) * Math.log(0.156);
     var dose = aucTransformada * cl;
     
     res.innerHTML = `O paciente apresenta Volume de Distribuição (Vd) igual a ${vd.toFixed(2).toString().replace('.', ',')}L 
